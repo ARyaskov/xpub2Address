df7f75057c6c2ee7e3845a318558de02ab24b25e
Andrei Riaskov <code@riaskov.com>
2024-06-14 18:29:15 +0300

    chore: respect lint

--- index.ts
+++ index.ts
@@ -15,27 +15,30 @@
  * Author: Andréi Riaskóv @ https://github.com/ARyaskov
  */
 
-import * as bjs from "bitcoinjs-lib";
-import * as BIP32 from 'bip32';
-import * as ecc from 'tiny-secp256k1';
-const bip32 = BIP32.BIP32Factory(ecc);
+import * as bjs from "bitcoinjs-lib"
+import * as BIP32 from "bip32"
+import * as ecc from "tiny-secp256k1"
+const bip32 = BIP32.BIP32Factory(ecc)
 
 export function xpub2Address(xpub: string, path: string) {
-  let result: string | undefined = ''
-  const ppa = path.split('/')
+  let result: string | undefined = ""
+  const ppa = path.split("/")
   let _network = bjs.networks.bitcoin
-  if (xpub.charAt(0) === 't') {
+  if (xpub.charAt(0) === "t") {
     _network = bjs.networks.testnet
   }
   const restored = bip32.fromBase58(xpub, _network)
   const purpose = ppa[1].slice(0, -1)
   const node = restored.derive(parseInt(ppa[4])).derive(parseInt(ppa[5]))
-  if (purpose === '49') {
+  if (purpose === "49") {
     result = bjs.payments.p2sh({
-      redeem: bjs.payments.p2wpkh({ pubkey: node.publicKey, network:  _network }),
-      network:  _network
+      redeem: bjs.payments.p2wpkh({
+        pubkey: node.publicKey,
+        network: _network
+      }),
+      network: _network
     }).address
-  } else if (purpose === '84') {
+  } else if (purpose === "84") {
     result = bjs.payments.p2wpkh({ pubkey: node.publicKey }).address
   } else {
     throw new Error(`BIP${purpose} is not implemented yet. Use 49 or 84.`)
